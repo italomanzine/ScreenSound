723501c144f6d70e43ea7b7c88e1a105f0a58710
italomanzine <italomanzine@gmail.com>
2024-01-08 15:20:25 -0300

    added switch case for options

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -4,9 +4,44 @@ string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
 void ExibirMensagemDeBoasVindas()
 {
-    Console.WriteLine("**************************");
+    Console.WriteLine(@"
+█▀ █▀▀ █▀█ █▀▀ █▀▀ █▄░█   █▀ █▀█ █░█ █▄░█ █▀▄
+▄█ █▄▄ █▀▄ ██▄ ██▄ █░▀█   ▄█ █▄█ █▄█ █░▀█ █▄▀");
     Console.WriteLine(mensagemDeBoasVindas);
-    Console.WriteLine("**************************");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
 }
 
 ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
+
+Console.WriteLine("\nDigite a sua opção: ");
+string opcaoEscolhida = Console.ReadLine()!;
+int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+switch (opcaoEscolhidaNumerica)
+{
+    case 1: 
+        Console.WriteLine("Você escolheu a opção: " + opcaoEscolhida);
+        break;
+    case 2:
+        Console.WriteLine("Você escolheu a opção: " + opcaoEscolhida);
+        break;
+    case 3:
+        Console.WriteLine("Você escolheu a opção: " + opcaoEscolhida);
+        break;
+    case 4:
+        Console.WriteLine("Você escolheu a opção: " + opcaoEscolhida);
+        break;
+    case -1:
+        Console.WriteLine("Programa Encerrado :)");
+        break;
+    default: Console.WriteLine("Opção Inválida");
+        break;
+}
