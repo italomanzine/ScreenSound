98a8cc58b0d67d177328c95ff587af0ad2627680
italomanzine <italomanzine@gmail.com>
2024-01-08 14:01:43 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿// Screen Sound
+
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine("**************************");
+    Console.WriteLine(mensagemDeBoasVindas);
+    Console.WriteLine("**************************");
+}
+
+ExibirMensagemDeBoasVindas();
